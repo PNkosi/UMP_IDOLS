05bb86db1e23a2369af0e0a6f823d396652502ca
Perfect Nkosi <89420688+PNkosi@users.noreply.github.com>
2022-08-22 12:50:46 +0200

    Update EmptyAliasException.java

--- src/com/perfectnkosi/EmptyAliasException.java
+++ src/com/perfectnkosi/EmptyAliasException.java
@@ -1,5 +1,9 @@
 package com.perfectnkosi;
 
+/**
+* @author Perfect NKosi__Codingd-dude
+*/
+
 public class EmptyAliasException extends Exception {
 
 	private static final long serialVersionUID = -699265432525316028L;
